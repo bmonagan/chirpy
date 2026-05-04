85837ecbad4cb95564a28d4433c71e7e631eded8
bmonagan <bernardmonagan@gmail.com>
2026-05-04 16:41:32 -0400

    Add health check endpoint and serve static files from /app directory

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.all('/healthz', (req, res) => {
+  console.log('Accessing the health check endpoint ...')
+  res.setHeader('Content-Type', 'text/plain');
+  return res.status(200).send('OK');
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
