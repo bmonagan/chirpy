a1b5fb56af43c420b600168d53c3ab01285a3ffd
bmonagan <bernardmonagan@gmail.com>
2026-05-04 21:40:06 -0400

    Refactor validateChirp function to improve response handling for chirp length validation

--- src/index.ts
+++ src/index.ts
@@ -60,9 +60,12 @@ async function validateChirp(req: Request, res: Response) {
   req.on("end", () => {
     try {
       const parsedBody = JSON.parse(body);
-      res.status(200).send({"valid": true})
+      if (parsedBody.length > 140) { 
+        return res.status(400).send("Chirp is too long.");
+      }
+      return res.status(200).send({"valid": true})
     } catch (error) {
-      res.status(400).send("Invalid JSON");
+      return res.status(400).send("Invalid JSON");
     }
   });
 }
