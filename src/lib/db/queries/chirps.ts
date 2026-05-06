55ad3b6702695ab625b3ef80077fcef33f598125
bmonagan <bernardmonagan@gmail.com>
2026-05-06 16:28:08 -0400

    add query to get chirp by id.

--- src/lib/db/queries/chirps.ts
+++ src/lib/db/queries/chirps.ts
@@ -1,6 +1,6 @@
 import { db } from "../../db/index.js";
 import { NewChirp, chirps } from "../../../schema.js";
-import { asc } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 
 export async function createChirp(chirp: NewChirp) {
   const [result] = await db
@@ -12,4 +12,11 @@ export async function createChirp(chirp: NewChirp) {
 
 export async function getChirps() {
   return await db.select().from(chirps).orderBy(asc(chirps.createdAt));
+}
+
+export async function getChirpById(id: string): Promise<typeof chirps.$inferSelect | null> {
+  const chirp = await db.query.chirps.findFirst({
+    where: eq(chirps.id, id),
+  });
+  return chirp ?? null;
 }
