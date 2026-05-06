975e42de527dc347904e372a037a9d2f891ec9e4
bmonagan <bernardmonagan@gmail.com>
2026-05-06 19:53:21 -0400

    add checkPasswordHash function

--- auth.ts
+++ auth.ts
@@ -1,4 +1,8 @@
-import {hash} from "argon2";
+import {hash, verify} from "argon2";
 export async function hashPassword(password: string): Promise<string> {
   return await hash(password);
+}
+
+export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
+  return await verify(password, hash);
 }
