029d8ef21ab78e230493b505cd7149ae083f1086
bmonagan <bernardmonagan@gmail.com>
2026-05-05 20:21:54 -0400

    Fix connection URL in db index to use dbConfig.url

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,5 +4,5 @@ import postgres from "postgres";
 import * as schema from "../../schema.js";
 import { chirpyConfig } from "../../config.js";
 
-const conn = postgres(chirpyConfig.dbURL);
+const conn = postgres(chirpyConfig.dbConfig.url);
 export const db = drizzle(conn, { schema });
