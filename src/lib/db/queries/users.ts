b6743f3950f408c50863c5cf750df7d9a04f61c4
bmonagan <bernardmonagan@gmail.com>
2026-05-06 20:01:16 -0400

    fix eq import to correctly be from drizzle

--- src/lib/db/queries/users.ts
+++ src/lib/db/queries/users.ts
@@ -1,5 +1,6 @@
-import { db,eq } from "../index.js";
+import { db } from "../index.js";
 import { NewUser, users } from "../../../schema.js";
+import { eq } from "drizzle-orm";
 
 export async function createUser(user: NewUser) {
   const [result] = await db
