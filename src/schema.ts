7b9a429f7270c15513be0cd00951502a417040c7
bmonagan <bernardmonagan@gmail.com>
2026-05-07 18:42:34 -0400

    add schema for refreshtokens table

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+import { create } from "node:domain";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -26,4 +27,14 @@ export const chirps = pgTable("chirps", {
     .references(() => users.id, { onDelete: "cascade" }),
 });
 
+export const refreshTokens = pgTable("refresh_tokens", {
+  token: varchar("token", { length: 256 }).primaryKey(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at"),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expiresAt: timestamp("expires_at").notNull(),
+  revokedAt: timestamp("revoked_at")
+});
 export type NewChirp = typeof chirps.$inferInsert;
