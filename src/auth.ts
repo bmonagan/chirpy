17945f116164d1969c0e733acb2921e0a8166749
bmonagan <bernardmonagan@gmail.com>
2026-05-06 20:43:35 -0400

    fix the order of variables in the verify functio from argon and renamed the hashed_password to not conflict with the hash.

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,6 @@ export async function hashPassword(password: string): Promise<string> {
   return await hash(password);
 }
 
-export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
-  return await verify(password, hash);
+export async function checkPasswordHash(password: string, hashed_password: string): Promise<boolean> {
+  return await verify(hashed_password, password);
 }
