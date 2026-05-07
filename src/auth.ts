7a98158cc78998a00a24b39f33b6c8214b207980
bmonagan <bernardmonagan@gmail.com>
2026-05-06 21:14:15 -0400

    add verifyJWT function

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import {hash, verify} from "argon2";
 import jwt from "jsonwebtoken";
 import type { JwtPayload } from "jsonwebtoken";
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
 export async function hashPassword(password: string): Promise<string> {
   return await hash(password);
 }
@@ -9,8 +12,15 @@ export async function checkPasswordHash(password: string, hashed_password: strin
   return await verify(hashed_password, password);
 }
 
-function makeJWT(userID: string, expiresIn: number, secret: string): string {
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
   return jwt.sign({ userID }, secret, { expiresIn });
 }
 
-type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+export function verifyJWT(token: string, secret: string): payload {
+  try {
+    const decoded = jwt.verify(token, secret) as payload;
+    return decoded;
+  } catch (err) {
+    throw new Error("Invalid token");
+  }
+}
