81511a2c489ee12152f3b708bf81154b8f8b0868
bmonagan <bernardmonagan@gmail.com>
2026-05-06 21:12:56 -0400

    add make jwt function

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,13 @@
 import {hash, verify} from "argon2";
+import jwt from "jsonwebtoken";
 export async function hashPassword(password: string): Promise<string> {
   return await hash(password);
 }
 
 export async function checkPasswordHash(password: string, hashed_password: string): Promise<boolean> {
   return await verify(hashed_password, password);
+}
+
+function makeJWT(userID: string, expiresIn: number, secret: string): string {
+  return jwt.sign({ userID }, secret, { expiresIn });
 }
